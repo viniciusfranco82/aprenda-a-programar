d02cd9bf8e612070a9c845b40d55a241412d1cb5
viniciusfranco82 <viniciusfranco82@outlook.com>
2022-08-24 20:10:24 -0300

    finaldetarefa2

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,8 +1,15 @@
-let primeiroNome = Vinicius
-let email = viniciusfranco82@outlook.com
-let profissao = educadorfisico
+let primeiroNome = "Vinicius"
+let email = "viniciusfranco82@outlook.com"
+let profissao = "educadorfisico"
 let idade = 40
-let viagemDoSonho = Dubai
+let viagemDoSonho = "Dubai"
 
+console.log(primeiroNome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(viagemDoSonho)
 
+let mensagem = "Olá, " + primeiroNome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + viagemDoSonho + ". Confirma pra gente alguns dados? A sua idade é " + idade + ", você é " + profissao + " e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
 
+console.log(mensagem)
