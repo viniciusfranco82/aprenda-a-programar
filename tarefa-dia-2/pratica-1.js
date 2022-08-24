8a2ed18032e0e28443bef26ab238fdc4b5eb4c5b
viniciusfranco82 <viniciusfranco82@outlook.com>
2022-08-24 14:07:24 -0300

    meu primeiro codigo

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,8 @@
-let = "nome"
-let = "email"
-let = "profissao"
-let = "idade"
-let = "viagemDoSonho"
+let primeiroNome = Vinicius
+let email = viniciusfranco82@outlook.com
+let profissao = educadorfisico
+let idade = 40
+let viagemDoSonho = Dubai
+
+
+
