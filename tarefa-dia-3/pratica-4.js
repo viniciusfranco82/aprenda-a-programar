cea4dabb379220971d9508cb3b9f0ae8978362fe
viniciusfranco82 <viniciusfranco82@outlook.com>
2022-08-28 18:35:52 -0300

    tarefa4

--- tarefa-dia-3/pratica-4.js
+++ tarefa-dia-3/pratica-4.js
@@ -6,9 +6,7 @@ let ehPrimo = true
 while (i <= n) {
     if (n % i == 0) {
         ehPrimo = false
-
-        break 
-    }
+}
     i++
 }
 
@@ -16,4 +14,3 @@ if (ehPrimo)
     console.log(`Sim, o número ${n} é primo.`)
 else
     console.log(`Não, o número ${n} não é primo.`)
-    
