13df828a70eceefe0b337c480a03df0350343e37
viniciusfranco82 <viniciusfranco82@outlook.com>
2022-08-28 18:02:03 -0300

    tarefa2

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,10 @@
+let multiplicador = 9
+let n = 10
+let i = 1
+
+while(i <= n) {
+let resultado = multiplicador * i
+let exibicao = `${multiplicador} x ${i} = ${resultado}`
+console.log (exibicao)
+i = i + 1
+}
